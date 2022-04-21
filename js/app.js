b2c2c130714303c7e09441b38d1a60f8cd81eb3b
Radwane69 <rnougha@edenschool.fr>
2022-04-21 16:04:26 +0200

    citation de Kanye west qui s'affiche

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-fetch("https://api.kanye.rest/")
-.then(reponse => reponse.json())
-.then(responce2 => console.log(responce2))
+let quotesDiv = document.getElementById('quotes')
+
+fetch('https://api.kanye.rest')
+.then(res => res.json())
+.then(quote => {
+    quotesDiv.innerHTML += `<p> ${quote.quote} </p>`
+})
+
+let Button = document.getElementById('give')
+
+Button.addEventListener("click", evt => {
+
+
+
+})
