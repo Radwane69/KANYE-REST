e037023d44d1dbf02b8266bb3e01eb514a1d7c64
Radwane69 <rnougha@edenschool.fr>
2022-04-22 14:00:28 +0200

    fini de faire un bouton qui affiche les phrases de kanye

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
-let quotesDiv = document.getElementById('quotes')
-
-fetch('https://api.kanye.rest')
-.then(res => res.json())
-.then(quote => {
-    quotesDiv.innerHTML += `<p> ${quote.quote} </p>`
-})
-
-let Button = document.getElementById('give')
-
-Button.addEventListener("click", evt => {
-
-
-
-})
+function getQuote() {
+    let kanye = new XMLHttpRequest();
+    kanye.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            let kanyeQuote = JSON.parse(this.responseText);
+            document.getElementById("kanye-quote").innerHTML = kanyeQuote.quote;
+        } else if(this.readyState != 4) {
+              document.getElementById("kanye-quote").innerHTML = "Loading...";
+        } else {
+              document.getElementById("kanye-quote").innerHTML = "Something went wrong..."
+        }
+    };
+  
+    kanye.open("GET", "https://api.kanye.rest/", true);
+    kanye.send();
+  
+  };
+  
+  document.getElementById("refresh").addEventListener("click", getQuote);
+  document.getElementById("refresh").addEventListener("click", kanyeSound);
