25646aba04d7428cdc5c2e28ea261c24adb5ebd9
Radwane69 <rnougha@edenschool.fr>
2022-05-10 15:46:45 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
-function getQuote() {
-    let kanye = new XMLHttpRequest();
-    kanye.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200) {
-            let kanyeQuote = JSON.parse(this.responseText);
-            document.getElementById("kanye-quote").innerHTML = kanyeQuote.quote;
-        } else if(this.readyState != 4) {
-              document.getElementById("kanye-quote").innerHTML = "Loading...";
-        } else {
-              document.getElementById("kanye-quote").innerHTML = "..."
-        }
-    };
-    kanye.open("GET", "https://api.kanye.rest/", true);
-    kanye.send();
-  };
-  document.getElementById("refresh").addEventListener("click", getQuote);
-  document.getElementById("refresh").addEventListener("click", kanyeSound);
+let quotesDiv = document.getElementById('quotes')
+
+fetch('https://api.kanye.rest')
+.then(res => res.json())
+.then(quote => {
+   console.log(quote.quote)
+})
+
+let Button = document.getElementById('give')
+
+Button.addEventListener("click", evt => {
+
+});
